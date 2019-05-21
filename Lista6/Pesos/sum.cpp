8e8fb015a9ec9d9a68310069df929d7b2c39784a
jeowf <moraisfelipe97@gmail.com>
2019-05-21 10:51:51 -0300

    lista6

---
+++ Lista6/Pesos/sum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 100000;
+
+int main(){
+	int sumx = 0;
+	int sumxx = 0;
+	int sumxy = 0;
+	int sumy = 0;
+
+	while (cin >> x >> y  ){
+		
+	}
+
+	cout << "1 " << sumx << " " << sumy << endl;
+	cout << sumx << " " << sumxx << " " << sumxy << endl;
+
+
+	return 0;
+}
